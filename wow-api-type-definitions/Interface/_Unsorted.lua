9dcdfd10ce01f7216eeb7714527de340fbfda698
Wht Mst <ven000mus@gmail.com>
2025-11-11 16:22:04 +0400

    Add files via upload

---
+++ wow-api-type-definitions/Interface/_Unsorted.lua
@@ -0,0 +1,31 @@
+-- Syntax for [declaring globals](https://github.com/LuaLS/lua-language-server/issues/116)
+
+BOOKTYPE_PET = "pet"
+BOOKTYPE_SPELL = "spell"
+
+---[Source](https://github.com/MOUZU/Blizzard-WoW-Interface/blob/master/1.12.1/FrameXML/ColorPickerFrame.xml)
+---[Documentation](https://wowpedia.fandom.com/wiki/Using_the_ColorPickerFrame?oldid=89462)
+---@class (exact) ColorPickerFrame : ColorSelect
+---@field cancelFunc fun(): nil
+---@field func fun(): nil
+---@field hasOpacity boolean
+---@field opacity number 0 to 1
+---@field previousValues [number, number, number] Values to apply on escape or close
+
+---@type ColorPickerFrame
+ColorPickerFrame = ColorPickerFrame
+
+---@type ScrollingMessageFrame
+DEFAULT_CHAT_FRAME = DEFAULT_CHAT_FRAME
+
+---@type Font
+GameFontNormalSmall = GameFontNormalSmall
+
+---@type GameTooltip
+GameTooltip = GameTooltip
+
+---@type table<string, function>
+SlashCmdList = SlashCmdList
+
+---@type Frame
+WorldFrame = WorldFrame
